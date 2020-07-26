b4406eaa45b63b7dc71be2f1106ec798f7f3a2cb
Chiara Rosa Velazco <chiara.rosavelazco@gmail.com>
2020-07-26 19:51:55 -0300

    Updates

---
+++ precache-manifest.998051723c7ec292181ac12580b76aab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b845cb6eb5c86d8c2e064b02c3ed5747",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "6c016f39242dab2a690c",
+    "url": "/repository-name/static/css/main.ae53a3d6.chunk.css"
+  },
+  {
+    "revision": "866bdc488ef668ef424b",
+    "url": "/repository-name/static/js/2.7224819c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repository-name/static/js/2.7224819c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c016f39242dab2a690c",
+    "url": "/repository-name/static/js/main.e92f7447.chunk.js"
+  },
+  {
+    "revision": "f636552da8476e65a70d",
+    "url": "/repository-name/static/js/runtime-main.9c95f0a6.js"
+  }
+]);
